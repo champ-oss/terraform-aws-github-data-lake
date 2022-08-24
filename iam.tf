f410a78b1db74a756ed57284c5879722b1d337ac
Justin Reeves <reeves122@gmail.com>
2022-08-24 14:07:52 -0500

    fix aws_iam_role_policy_attachment resource name

--- iam.tf
+++ iam.tf
@@ -18,7 +18,7 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "this" {
+resource "aws_iam_role_policy_attachment" "AmazonKinesisFirehoseFullAccess" {
   role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonKinesisFirehoseFullAccess"
 }
