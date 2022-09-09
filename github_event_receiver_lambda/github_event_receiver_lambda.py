c2cdfc056e5d7e707dcc62bd60af06092aa4c5c3
Justin Reeves <reeves122@gmail.com>
2022-09-09 13:02:13 -0500

    add docstrings and unit tests

--- github_event_receiver_lambda/github_event_receiver_lambda.py
+++ github_event_receiver_lambda/github_event_receiver_lambda.py
@@ -5,33 +5,48 @@ from typing import Dict, Any
 
 import boto3
 
-BODY_KEY = os.getenv('BODY_KEY', 'body')
-HEADERS_KEY = os.getenv('HEADERS_KEY', 'headers')
-SIGNATURE_HEADER_KEY = os.getenv('SIGNATURE_HEADER_KEY', '')
-SHARED_SECRET = os.getenv('SHARED_SECRET', '')
-ENCODING = os.getenv('ENCODING', 'utf-8')
-SNS_TOPIC_ARN = os.getenv('SNS_TOPIC_ARN')
 SNS_CLIENT = boto3.client('sns', region_name=os.getenv('AWS_REGION'))
 
 
-def handler(event, _):
+def handler(event: Dict[str, Any], _) -> dict:
+    """
+    entrypoint for AWS Lambda function
+
+    :param event: AWS Lambda event
+    :param _: unused context
+    :return: dict of HTTP response information
+    """
     print('event:', event)
-    if not _is_signature_valid(event):
+    if not _is_signature_valid(event, os.getenv('SHARED_SECRET', '')):
         return _create_response(401)
 
-    _publish(event.get(BODY_KEY))
+    _publish(SNS_CLIENT, os.getenv('SNS_TOPIC_ARN'), event.get('body'))
     return _create_response(200)
 
 
-def _is_signature_valid(event: Dict[str, Any]) -> bool:
-    signature = hmac.new(SHARED_SECRET.encode(ENCODING),
-                         msg=event.get(BODY_KEY, '').encode(ENCODING),
+def _is_signature_valid(event: Dict[str, Any], shared_secret: str) -> bool:
+    """
+    verifies that the signature in the request header matches the
+    actual signature of the request payload
+
+    :param event: AWS Lambda event
+    :param shared_secret: secret used to sign the message payload
+    :return: true or false
+    """
+    signature = hmac.new(shared_secret.encode('utf-8'),
+                         msg=event.get('body', '').encode('utf-8'),
                          digestmod=hashlib.sha256).hexdigest()
     print('calculated signature:', signature)
-    return signature == event.get(HEADERS_KEY, {}).get(SIGNATURE_HEADER_KEY)
+    return signature == event.get('headers', {}).get('x-hub-signature-256')
 
 
 def _create_response(status_code: int) -> Dict[str, int]:
+    """
+    generate a dict containing HTTP response information
+
+    :param status_code: HTTP status code to use
+    :return: dict of HTTP response information
+    """
     response = {
         "statusCode": status_code
     }
@@ -39,10 +54,19 @@ def _create_response(status_code: int) -> Dict[str, int]:
     return response
 
 
-def _publish(message: str) -> None:
-    print('publishing to SNS topic:', SNS_TOPIC_ARN)
-    publish_response = SNS_CLIENT.publish(
-        TopicArn=SNS_TOPIC_ARN,
+def _publish(sns_client: boto3.client, topic_arn: str, message: str) -> dict:
+    """
+    publish the message to the SNS topic
+
+    :param sns_client: initialized boto3 SNS client
+    :param topic_arn: which SNS topic to send to
+    :param message: body of message to publish to SNS
+    :return: response from SNS publish operation
+    """
+    print('publishing to SNS topic:', topic_arn)
+    publish_response = sns_client.publish(
+        TopicArn=topic_arn,
         Message=message
     )
     print('publish_response', publish_response)
+    return publish_response
--- github_event_receiver_lambda/test_github_event_receiver_lambda.py
+++ github_event_receiver_lambda/test_github_event_receiver_lambda.py
@@ -7,18 +7,29 @@ class Test(TestCase):
     github_event_receiver_lambda = None
 
     def setUp(self) -> None:
-        os.environ['AWS_REGION'] = 'us-east-1'
-        os.environ['SIGNATURE_HEADER_KEY'] = 'x-hub-signature-256'
+        os.environ['AWS_REGION'] = 'test'
         os.environ['SHARED_SECRET'] = 'testing123'
         import github_event_receiver_lambda as github_event_receiver_lambda
         self.github_event_receiver_lambda = github_event_receiver_lambda
+        self.github_event_receiver_lambda.SNS_CLIENT.publish = MagicMock(return_value='test')
 
-    # def test_handler(self):
-    #     os.environ['AWS_REGION'] = 'us-east-1'
-    #     import github_event_receiver_lambda as github_event_receiver_lambda
-    #     github_event_receiver_lambda.SNS_CLIENT.publish = MagicMock(return_value="test")
-    #     result = github_event_receiver_lambda.handler(None, None)
-    #     self.assertEqual("test", result)
+    def test_handler_returns_200_with_valid_signature(self):
+        event = {
+            'headers': {
+                'x-hub-signature-256': '062ef9199de74587dc0ff97d6273f0a6734e0346d48669a7e35b636c12171a7c'
+            },
+            'body': "test123"
+        }
+        self.assertEqual({'statusCode': 200}, self.github_event_receiver_lambda.handler(event, None))
+
+    def test_handler_returns_401_with_invalid_signature(self):
+        event = {
+            'headers': {
+                'x-hub-signature-256': 'foo123'
+            },
+            'body': "test123"
+        }
+        self.assertEqual({'statusCode': 401}, self.github_event_receiver_lambda.handler(event, None))
 
     def test__is_signature_valid_returns_true_with_valid_signature(self):
         event = {
@@ -27,7 +38,7 @@ class Test(TestCase):
             },
             'body': "test123"
         }
-        self.assertTrue(self.github_event_receiver_lambda._is_signature_valid(event))
+        self.assertTrue(self.github_event_receiver_lambda._is_signature_valid(event, 'testing123'))
 
     def test__is_signature_valid_returns_false_with_invalid_signature(self):
         event = {
@@ -36,26 +47,32 @@ class Test(TestCase):
             },
             'body': "test123"
         }
-        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid(event))
+        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid(event, 'testing123'))
 
     def test__is_signature_valid_returns_false_with_missing_header(self):
         event = {
             'headers': {},
             'body': "test123"
         }
-        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid(event))
+        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid(event, 'testing123'))
 
     def test__is_signature_valid_returns_false_with_missing_event(self):
-        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid({}))
+        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid({}, 'testing123'))
 
-    # def test__is_signature_valid_returns_false_with_env_not_set(self):
-    #     event = {
-    #         'headers': {
-    #             'x-hub-signature-256': '062ef9199de74587dc0ff97d6273f0a6734e0346d48669a7e35b636c12171a7c'
-    #         },
-    #         'body': "test123"
-    #     }
-    #     import github_event_receiver_lambda as github_event_receiver_lambda
-    #     github_event_receiver_lambda.SIGNATURE_HEADER_KEY = ''
-    #     github_event_receiver_lambda.SHARED_SECRET = ''
-    #     self.assertFalse(github_event_receiver_lambda._is_signature_valid(event))
+    def test__is_signature_valid_returns_false_with_empty_secret(self):
+        event = {
+            'headers': {
+                'x-hub-signature-256': '062ef9199de74587dc0ff97d6273f0a6734e0346d48669a7e35b636c12171a7c'
+            },
+            'body': "test123"
+        }
+        self.assertFalse(self.github_event_receiver_lambda._is_signature_valid(event, ''))
+
+    def test__create_response(self):
+        self.assertEqual({
+            "statusCode": 200
+        }, self.github_event_receiver_lambda._create_response(200))
+
+    def test__publish(self):
+        self.assertEqual('test', self.github_event_receiver_lambda._publish(
+            self.github_event_receiver_lambda.SNS_CLIENT, 'topic123', 'message'))
