6cfe3f4df906cdfeb52d55d10fa27fc068155a9d
Justin Reeves <reeves122@gmail.com>
2023-09-26 16:16:19 -0500

    test without terratest

---
+++ examples/complete/test.sh
@@ -0,0 +1,10 @@
+set -e
+
+SHARED_SECRET="testing123"
+POST_DATA='{"action":"completed","number":"54"}'
+HMAC=$(echo -n "${POST_DATA}" | openssl dgst -sha256 -hmac "${SHARED_SECRET}" | cut -d ' ' -f 2)
+HMAC_HEADER="x-hub-signature-256: sha256=${HMAC}"
+CT_HEADER="Content-Type: application/json; charset=UTF-8"
+
+curl -X POST -H $CT_HEADER -H $HMAC_HEADER -d $POST_DATA $FUNCTION_URL
+
