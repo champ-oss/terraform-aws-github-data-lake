c1f8b51c0ec500609d6d34dafc0ca2004ef96cdd
Justin Reeves <reeves122@gmail.com>
2023-09-26 16:39:16 -0500

    increase wait time

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -6,9 +6,11 @@ HMAC=$(echo -n "${POST_DATA}" | openssl dgst -sha256 -hmac "${SHARED_SECRET}" | 
 HMAC_HEADER="x-hub-signature-256: sha256=${HMAC}"
 CT_HEADER="Content-Type: application/json; charset=UTF-8"
 
-curl -v -X POST -H "${CT_HEADER}" -H "${HMAC_HEADER}" -d "${POST_DATA}" $FUNCTION_URL
+echo -e "\nTesting two HTTP POST curl requests..."
+curl -s -X POST -H "${CT_HEADER}" -H "${HMAC_HEADER}" -d "${POST_DATA}" $FUNCTION_URL
+curl -s -X POST -H "${CT_HEADER}" -H "${HMAC_HEADER}" -d "${POST_DATA}" $FUNCTION_URL
 
 echo -e "\nWaiting for S3 objects..."
-sleep 60
+sleep 120
 aws s3 ls s3://$BUCKET
 
